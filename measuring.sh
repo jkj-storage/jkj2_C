aa26c44e4c91fca2625a5a7456e51d9ee720f37c
jkj-storage <s2331097tj@s.chibakoudai.jp>
2024-11-17 16:33:29 +0900

    Update measuring.sh

--- measuring.sh
+++ measuring.sh
@@ -2,8 +2,8 @@
 #measuring.sh
 
 if [ "${#}" -ne 4 ]; then
-	echo "Usage: ${0} {targetURL} {outputFilename} {concurrency} {repeatNum}"
-	exit 1
+        echo "Usage: ${0} {targetURL} {outputFilename} {concurrency} {repeatNum}"
+        exit 1
 fi
 
 URL=${1}
@@ -22,18 +22,19 @@ sleep 3
 
 for request in $(seq 10 10 100) $(seq 200 100 1000) # $(seq 2000 1000 10000)
 do
-	sleep 2
-	sum=0
-	echo "====== ${request} request ======" 
-	for cnt in $(seq 1 ${repeat_num})
-	do
-		res=$(ab -c ${multi_access} -n ${request} ${URL} \
-  			| grep 'Requests per second' \
-     			| sed -r 's/.* ([0-9]+)(\.)([0-9]+).*/\1\2\3/')
-		sum=$(echo "scale=2; ${sum} + ${res}" | bc)
-	done
-	
-	avg=$(echo "scale=2; ${sum} / ${repeat_num}" | bc)
-	echo "${request} ${avg}" >> ${filename}
-	echo "sum:${sum} | average:${avg}"
+        sleep 2
+        sum=0
+        echo "====== ${request} request ======"
+        for cnt in $(seq 1 ${repeat_num})
+        do
+                res_buf=$(ab -c ${multi_access} -n ${request} ${URL} \
+                        | grep 'Requests per second')
+                res=$(echo "${res_buf}" | sed -r 's/.* +([0-9]+)(\.)([0-9]+).*/\1\2\3/')
+                sum=$(echo "scale=2; ${sum} + ${res}" | bc)
+        done
+
+        avg=$(echo "scale=2; ${sum} / ${repeat_num}" | bc)
+        echo "${request} ${avg}" >> ${filename}
+        echo "sum:${sum} | average:${avg}"
+        echo "res_buf: ${res_buf}"
 done
